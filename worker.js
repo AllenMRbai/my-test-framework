281319778c714660c926fea4083078308875e61f
bairongyu <957820957@qq.com>
2022-03-10 17:53:38 +0800

    3-5实现一个断言框架

--- worker.js
+++ worker.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+// expect方法内部有很多功能和特性，如果都写到这个文件里代码会变得很乱，我们将它抽取到单独模块内
+// 这里的expect用到的是jest作者实现的库
+const expect = require("expect");
 
 exports.runTest = async function(testFile) {
   const code = await fs.promises.readFile(testFile, "utf8");
@@ -7,16 +10,6 @@ exports.runTest = async function(testFile) {
     errorMessage: null,
   };
   try {
-    // 实现expect方法
-    // eval内运行的代码能够读取到worker.js的上下文，所以 ReferenceError: expect is not defined 就没了
-    const expect = (received) => ({
-      toBe: (expected) => {
-        if (received !== expected) {
-          throw new Error(`Expected ${expected} but received ${received}.`);
-        }
-        return true;
-      },
-    });
     eval(code);
     testResult.success = true;
   } catch (error) {
