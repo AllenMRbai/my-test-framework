c0a26bd1679716f9cec0a30f96de8f9e8c41df1f
bairongyu <957820957@qq.com>
2022-03-10 17:57:10 +0800

    3-6实现一个断言框架

--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
-// expect方法内部有很多功能和特性，如果都写到这个文件里代码会变得很乱，我们将它抽取到单独模块内
-// 这里的expect用到的是jest作者实现的库
 const expect = require("expect");
+// 我们测试的过程中可能需要用到测试框架的mock共
+// 这个功能我们就不去实现了，直接使用jest作者开发的jest-mock
+const mock = require("jest-mock");
 
 exports.runTest = async function(testFile) {
   const code = await fs.promises.readFile(testFile, "utf8");
