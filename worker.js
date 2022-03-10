cc45689cb62482527819678f2620ae6b940ce3b3
bairongyu <957820957@qq.com>
2022-03-10 17:41:48 +0800

    3-2实现一个断言框架

--- worker.js
+++ worker.js
@@ -2,7 +2,10 @@ const fs = require("fs");
 
 exports.runTest = async function(testFile) {
   const code = await fs.promises.readFile(testFile, "utf8");
-  // 这里暂时用eval来运行需要测试的代码
-  // 现在运行它应该会报 ReferenceError: expect is not defined ,因为上下文里没有expect方法。
-  eval(code);
+  // Let’s add a guard to the `eval` call so that a single failing test cannot bring down our whole test framework
+  try {
+    eval(code);
+  } catch (error) {
+    // Something went wrong.
+  }
 };
