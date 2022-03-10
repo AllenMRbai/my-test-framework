0a02e35a358c39735f00e9206928d71baac7d886
bairongyu <957820957@qq.com>
2022-03-10 19:01:32 +0800

    7-2实现require和module.export

--- worker.js
+++ worker.js
@@ -2,25 +2,40 @@ const fs = require("fs");
 const expect = require("expect");
 const mock = require("jest-mock");
 const { describe, it, run, resetState } = require("jest-circus");
-// 很幸运，node提供了vm模块，可以用来做沙盒
 const vm = require("vm");
+const NodeEnvironment = require("jest-environment-node");
+const { dirname, basename, join } = require("path");
 
 exports.runTest = async function(testFile) {
-  const code = await fs.promises.readFile(testFile, "utf8");
   const testResult = {
     success: false,
     errorMessage: null,
   };
   try {
     resetState();
-    // setTimeout是worker.js的上线文内容，我们没有通过context传到vm里面，所以沙箱里访问不到。
-    // 同样的，所有通过上下文访问的变量都需要传到沙箱内，所以jest作者整合里一个 jest-environment-node ，将所有要用到的变量都整合一起
-    const NodeEnvironment = require("jest-environment-node");
-    const environment = new NodeEnvironment({
-      testEnvironmentOptions: { describe, it, expect, mock },
+    let environment;
+    const customRequire = (fileName) => {
+      const code = fs.readFileSync(join(dirname(testFile), fileName), "utf8");
+      const moduleFactory = vm.runInContext(
+        // Inject require as a variable here.
+        `(function(module, require) {${code}})`,
+        environment.getVmContext()
+      );
+      const module = { exports: {} };
+      // And pass customRequire into our moduleFactory.
+      moduleFactory(module, customRequire);
+      return module.exports;
+    };
+    environment = new NodeEnvironment({
+      testEnvironmentOptions: {
+        describe,
+        it,
+        expect,
+        mock,
+      },
     });
-    vm.runInContext(code, environment.getVmContext());
-
+    // Use `customRequire` to run the test file.
+    customRequire(basename(testFile));
     const { testResults } = await run();
     testResult.testResults = testResults;
     testResult.success = testResults.every((result) => !result.errors.length);
