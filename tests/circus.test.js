8298b42ad0aa818c3d9cd6e7c849024c0acc1cb9
bairongyu <957820957@qq.com>
2022-03-10 18:54:54 +0800

    7-1实现require和module.export

--- tests/circus.test.js
+++ tests/circus.test.js
@@ -1,6 +1,8 @@
-describe("second circus test", () => {
-  it(`doesn't work`, async () => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    expect(1).toBe(2);
-  });
+const banana = require("./banana.js");
+
+// 运行这个test，会报错 require is not defined
+// 因为我们没有将require和module传个沙盒
+// 但我们不能只讲将worker.js内的require传给沙盒，因为worker.js和当前测试的文件不在同一个路径。
+it("tastes good", () => {
+  expect(banana).toBe("good");
 });
