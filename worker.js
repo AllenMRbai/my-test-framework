96c008000d127f62d07fdef9bf7a12ff6839e33c
bairongyu <957820957@qq.com>
2022-03-10 18:25:26 +0800

    5-3实现describe和it

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const expect = require("expect");
 const mock = require("jest-mock");
 // 同样的，我们将 describe 和 it 封装成一个模块
-const { describe, it, run } = require("jest-circus");
+const { describe, it, run, resetState } = require("jest-circus");
 
 exports.runTest = async function(testFile) {
   const code = await fs.promises.readFile(testFile, "utf8");
@@ -11,6 +11,8 @@ exports.runTest = async function(testFile) {
     errorMessage: null,
   };
   try {
+    // 每个测试文件间的state是共享的，我们需要手动reset掉
+    resetState();
     eval(code);
     // node index.mjs circus
     const { testResults } = await run();
